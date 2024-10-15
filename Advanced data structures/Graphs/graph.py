9b970739dc28d25f20d97a69c92b8586d8dbbf99
Salazhiev <112008148+Salazhiev@users.noreply.github.com>
2024-10-15 08:45:07 +0300

    Update graph.py

--- Advanced data structures/Graphs/graph.py
+++ Advanced data structures/Graphs/graph.py
@@ -21,6 +21,13 @@ class Vertex:
 
 # Сам граф.
 class Graph:
+    # Количесто вершин.
+    len_vertex = 0
+    # Количество ребер.
+    len_edge = 0
+    # Матрица смежности.
+    matrix = []
+
     def __init__(self, oriented = False):
         # Массив вершин графа. В этом массиве хранятся объекты класса Vertex.
         self.V = []
@@ -32,31 +39,60 @@ class Graph:
 
 
     # Метод для добавления вершин в наш граф.
+    # Понятно, что можно по манипулировать с этим методом, чтобы он не принимал на вход
+    # сами непосредственно объекты класса Vertex, а в этом методе, их создавать и добавлять
+    # в наш класс.
     def addVertex(self, v):
-
+        # Получаем объект класса Vertex, добавляем его просто в массив вершин.
         self.V.append(v)
+        self.len_vertex += 1
+
+        # обновляем матрицу смежности.
+        self.matrix.append([0]*self.len_vertex)
+        for i in range(self.len_vertex):
+            self.matrix[i].append(0)
+
 
 
 
     # Метод для добавления ребер в наш граф.
+    # На вход принимает два объекта класса Edge, откуда и куда.
     def addEdge(self, from_, to_):
+        # Вспомогательный флаг, для неориентированного графа, а если точнее
+        # не произошла хрень, при добавлении элементов.
+        flag = True
+        self.len_edge += 1
+
+        # Создаем экземпляр класса Edge - ребро
         edge = Edge(from_, to_)
-        self.E.append(edge)
 
+        # Если наш граф неориентированный, то добавляем как один путь, так и второй.
+        if self.oriented == False:
+            edge_ = Edge(to_, from_)
 
+            # Важно учитывать также, что мы случайно можем добавить один и тот же путь дважды, поэтому
+            # Этот момент тоже надо контролировать.
+            for e in self.E:
+                if e.from_ is edge_.from_ and e.to_ is edge_.to_:
+                    flag = False
+            if flag:
+                # Обновляем матрицу смежности.
+                self.matrix[edge_.from_.name-1][edge_.to_.name-1] = edge_.weight
+                # Добавляем в массив ребер, новое ребро.
+                self.E.append(edge_)
 
-    # Генерирует матрицу смежности.
-    def getMatrixGtaphs(self):
-        # Матрица смежности, которую будем строить.
-        matrix = [ [0]*len(self.V) for _ in range(len(self.V)) ]
+        # Опять проверка на случай, если наш граф ориентированнный.
+        flag = True
+        if self.oriented==False:
+            for e in self.E:
+                if e.from_ is edge.from_ and e.to_ is edge.to_:
+                    flag = False
+        if flag:
+            # Обновляем матрицу смежности.
+            self.matrix[edge.from_.name-1][edge.to_.name-1] = edge.weight
+            # Добавляем в массив ребер, новое ребро.
+            self.E.append(edge)
 
-        # Перебираем наши ребра.
-        for edge in self.E:
-            i, j = edge.from_.name, edge.to_.name
-            matrix[i-1][j-1] = edge.weight
-
-        for a in matrix:
-            print(a)
 
 
     # Генерирует список смежных вершин.
@@ -65,21 +101,23 @@ class Graph:
             self.getOneVertexLists(vertex)
 
 
+
     # Генерирует список смежных вершин с данной.
     def getOneVertexLists(self, vertex):
         # Передаем вершину и получаем все смежные с ней.
         result = []
-        # print(vertex.name, end=': ')
+        print(vertex.name, end=': ')
         for edge in self.E:
             if edge.from_ is vertex:
-                # print(edge.to_.name, end=' ')
+                print(edge.to_.name, end=' ')
                 result.append(edge.to_)
-        # print()
+        print()
 
         return result
 
 
     # Волновой алгоритм поиска пути от A до B графе.
+    # Насколько я понял, это также обход графа в ширину.
     def wave(self, start_, finish_):
 
         v = [start_]
@@ -92,15 +130,54 @@ class Graph:
                     for vertex in self.getOneVertexLists(v_):
                         if vertex is finish_:
                             return True
-                        vn.append(vertex)
+
+                        # Данное условие нужно для неориентированного графа.
+                        if self.oriented==False and vertex not in visited_vertex:
+                            vn.append(vertex)
+                        elif self.oriented==True:
+                            vn.append(vertex)
             v = vn
 
         return False
 
 
+    # Обход графа в глубину для поиска элемента в графе.
+    # ПОЕБНЯ НЕ РАБОТАЕТ ПОКА ЧТО, НУЖНО ИСПРАВИТЬ ХРЕНЬ В ОБЩЕМ.
+    def search(self, s, f, arr):
+        if s is f:
+            return True
+
+
+        for vertex in self.getOneVertexLists(s):
+            if vertex is arr:
+                continue
+
+            arr.append(vertex)
+            return self.search(vertex, f, arr)
+
+
+
+
+
+
+
     # Волновой алгоритм поиска пути от A до B в графе с выводом пути.
     def wave_road(self, s, f):
         pass
+
+
+    def __len__(self):
+        flag = 'Ориентированном'
+        if self.oriented == False:
+            flag = 'Неориентированном'
+        return f'Количество вершин в {flag}:{self.len_vertex},  ребер{self.len_edge}'
+
+
+    def __str__(self):
+        result = ''
+        for a in self.matrix:
+            result += str(a)+'\n'
+        return result
 
 
 
@@ -129,9 +206,12 @@ g.addEdge(v2, v5)
 g.addEdge(v2, v6)
 g.addEdge(v6, v5)
 g.addEdge(v5, v6)
+g.addEdge(v1, v7)
 
-g.getMatrixGtaphs()
 g.getVertexLists()
 
-print()
-print(g.wave(v1, v7))
+
+print(g)
+print(g.wave(v1, v6))
+
+print(g.search(v1, v4, []))
